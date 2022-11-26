9ed7d14b6ecf2595a0c125e6fe71aa027902a5e5
qurratulainiazzakiyah <100498086+qurratulainiazzakiyah@users.noreply.github.com>
2022-11-26 13:01:03 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,34 +1,38 @@
 <html>
-    <body>
-    welcome <?php echo $_POST["firstname"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+<body>
 
-    $servername="localhost";
-    $username="root";
-    $password="";
+Welcome <?php echo $_POST["firstname"]; ?><br>
+Your Email adress is: <?php echo $_POST["email"]; ?>
 
-    $dbname="mydb";
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-   
-    // Create Connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check Connection
-    if ($conn->connect_error) {
-        die("Connection Failed: ". $conn -> connect_error);
-    }
-    $sql = "INSERT INTO MYGuests (firstname, lastname, email)
-    VALUE ('$Hari','$Tanggal','$Kegiatan','$curah_hujan', '$Penjualan')";
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="mydb";
+$firstname=$_POST["firstname"];
+$lastname=$_POST["lastname"];
+$email=$_POST["email"];
 
-    if ($conn -> query($sql) === TRUE) {
-        echo "New Records Created Successfully";
-    } else {
-        echo 'Error' . $sql . "<br>". $conn->error;
-    }
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed:" . $conn->connect_error);
+}
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $sql = "SELECT id, firstname, lastname FROM MyGuests"
-    ?>
-    </body>
-</html>
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('$firstname', '$lastname', '$email')";
+
+if ($conn -> query($sql) === TRUE) {
+    echo "New records created succesfully";
+} else {
+    echo "Error: ". $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+<br>
+<a href="http:/localhost/daftar.php">Data yang terdaftar</a>
+
+</body>
+</html> 
